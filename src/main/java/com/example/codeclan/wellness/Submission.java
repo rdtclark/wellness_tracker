c04124e2a1413c0673c1abb088336b6c195c4361
Nathan <nathankirley@gmail.com>
2020-07-10 21:13:27 +0100

    initial commit

---
+++ src/main/java/com/example/codeclan/wellness/Submission.java
@@ -0,0 +1,4 @@
+package com.example.codeclan.wellness;
+
+public class Submission {
+}
---
+++ src/test/java/com/example/codeclan/wellness/WellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.wellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
