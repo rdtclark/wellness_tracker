a13f5506ad91a62ab0c1704e68fcd3b8a53354bc
Nathan <nathankirley@gmail.com>
2020-07-12 11:15:32 +0100

    can view answers and submissions on browser

--- src/main/java/com/example/codeclan/wellness/controllers/SubmissionController.java
+++ src/main/java/com/example/codeclan/wellness/controllers/SubmissionController.java
@@ -1,4 +1,29 @@
 package com.example.codeclan.wellness.controllers;
 
+import com.example.codeclan.wellness.models.Submission;
+import com.example.codeclan.wellness.repositories.SubmissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class SubmissionController {
+
+    @Autowired
+    SubmissionRepository submissionRepository;
+
+    @GetMapping("/submissions")
+    public ResponseEntity<List<Submission>> getAllSubmissions(){
+        return new ResponseEntity<>(submissionRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/submissions/{id}")
+    public ResponseEntity getSubmission(@PathVariable Long id){
+        return new ResponseEntity<>(submissionRepository.findById(id), HttpStatus.OK);
+    }
 }
