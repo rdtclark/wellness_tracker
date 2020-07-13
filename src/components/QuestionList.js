b326739beff0045f139a1277b84a0b3527636ce3
Robin Clark <robindtclark@gmail.com>
2020-07-13 09:19:09 +0100

    refactor questions form into one component

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import Question from './Question';
-import Reason from './Reason';
 
 class QuestionList extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            answers: []
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            answer5: "",
+            answer6: "",
+            dayComment: ""
         }
     }
 
     render() {
 
         const questions = this.props.questionList.map(question => {
-          return <Question
-            question_content={question.content}
-            key={question.id}
-            id={question.id}>
-            </Question>
+
+            if (!question.content && !question.id) return null;
+
+            return <div key={question.id}>
+            <p>
+            {question.content}
+            </p>
+            <input
+             
+            type="text" 
+            placeholder="Answer" 
+            id={question.id}
+            />
+            </div>
         
         });
         
@@ -26,7 +39,13 @@ class QuestionList extends Component {
             <>
                 <form className="question-form">      
                 {questions}
-                <Reason/>
+                <p>Good/Bad Day Reason [Box]</p>
+                <input 
+                type="text" 
+                placeholder="Answer" 
+                id="day-comment"
+                />
+                <input type="submit" value="Submit" />
                 </form>
             </>
         )
