c32d9ae91f608ef769fa8d244595d2ffca326e5b
nmackenzie91 <nmackenzie@live.co.uk>
2020-07-13 15:13:39 +0100

    handlers added for reasonbydate form

--- src/components/ReasonByDate.js
+++ src/components/ReasonByDate.js
@@ -5,24 +5,72 @@ class ReasonByDate extends Component {
     constructor(props){
         super(props);
         this.state={
+            startDate: '',
+            endDate: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleStartDateChange = this.handleStartDateChange.bind(this);
+        this.handleEndDateChange = this.handleEndDateChange.bind(this);
+    }
 
+    handleSubmit(event){
+        event.preventDefault();
+
+        const startDate = this.state.startDate;
+        const endDate = this.state.endDate;
+        if (!startDate || !endDate){
+            return
         }
+
+        this.props.onDateSubmit({
+            startDate: startDate,
+            endDate: endDate
+        })
+
+        this.setState({
+            startDate: '',
+            endDate: ''
+        })
     }
 
 
+    handleEndDateChange(event){
+        this.setState({
+            startEnd: event.target.value
+        })
+    }
 
+    handleStartDateChange(event){
+        this.setState({
+            startDate: event.target.value
+        })
+    }
 
     render(){
         return(
             <div>
-                <b><h1>Select a date</h1></b>
-            <form>
+                <h1><b>Select a date</b></h1>
+            <form onSubmit={this.handleSubmit}>
+                <label for="sdate">Start Date</label>
                 <input
                 type="date"
-                placeholder="Start"/>
+                placeholder="Start Date"
+                name="sdate"
+                value={this.state.startDate}
+                onChange={this.handleStartDateChange}/>
+
+
+                <label for="edate">End Date</label>
                 <input
                 type="date"
-                placeholder="End"/>
+                placeholder="End Date"
+                name="edate"
+                value={this.start.endDate}
+                onChange={this.handleEndDateChange}/>
+
+                <input
+                type="submit"
+                value="Get"/>
             </form>
             </div>
         )
