8f157c570f8d888418971356406d7dad8203b4d6
nmackenzie91 <nmackenzie@live.co.uk>
2020-07-13 10:13:54 +0100

    if statement to filter between good/bad works

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -6,20 +6,40 @@ const ReviewList = ({previousResults}) => {
 
     const goodReasons = previousResults.map(reason => {
 
+        if (reason.dayScore > 3){ 
         return (
             <GoodReason
             reason={reason.dayComment}
             key={reason.id}>
 
             </GoodReason>
-        )
+        )} 
+    })
+
+    const badReasons = previousResults.map(reason => {
+
+        if (reason.dayScore < 3){ 
+        return (
+            <GoodReason
+            reason={reason.dayComment}
+            key={reason.id}>
+
+            </GoodReason>
+        )} 
     })
 
 
     return(
+        <>
         <div>
+            <h3>Good</h3>
             <p>{goodReasons}</p>
         </div>
+        <div>
+            <h3>Bad</h3>
+            <p>{badReasons}</p>
+        </div>
+        </>
     )
 
 }
