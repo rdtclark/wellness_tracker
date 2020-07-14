c5feab284359bd5a605cdfbdfcd6cce374d8c954
Robin Clark <robindtclark@gmail.com>
2020-07-14 07:38:47 +0100

    add header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = ({}) => {
+
+
+    return (
+        <>
+            <footer>
+                <h2>I am the footer</h2>
+            </footer>
+        </>
+
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ({}) => {
+
+
+    return (
+        <>
+            <header>
+                <h2>I am the header</h2>
+            </header>
+        </>
+
+    )
+}
+
+export default Header;
