2bd78695477320a4878e8c9c7808c4020a406f46
Valerian Roustit <valerian.roustit@gmail.com>
2020-07-15 13:36:54 +0100

    update stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -17,17 +17,17 @@ const Stats = ({submissionsData}) => {
     // console.log(data[i]);
     stats.dates.push(new Date(submissionsData[i].date));
     stats.dayScores.push(submissionsData[i].dayScore);
-    stats.sleepScores.push(submissionsData[i].answers);
+    stats.sleepScores.push(submissionsData[i].answers[1].score);
     stats.eatScores.push(submissionsData[i].answers[1].score);
     stats.mentalScores.push(submissionsData[i].answers[2].score);
     stats.socialScores.push(submissionsData[i].answers[3].score);
     stats.physicalScores.push(submissionsData[i].answers[4].score);
   }
 
-  const data = [["Date", "Day score"]];
+  const data = [["Date", "Day score", "Sleep score", "Eat score", "Mental score", "Social score", "Physical score"]];
 
   for(let i = 0 ; i < stats.dates.length ; i++ ) {
-    data.push([stats.dates[i], stats.dayScores[i]]);
+    data.push([stats.dates[i], stats.dayScores[i], stats.sleepScores[i], stats.eatScores[i], stats.mentalScores[i], stats.socialScores[i], stats.physicalScores[i]]);
   }
 
   return (
